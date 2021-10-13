e4473ec6a7149701147f886387d5511cef168162
Rakesh Padashetty <p.rakesh34@gmail.com>
2021-10-13 22:48:43 +0530

    finished UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
+import { Button, Container, Form, Grid, GridRow, Icon, Segment, Statistic } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <header as="h1"> Budget</header>
+
+      <Statistic size="small">
+        <Statistic.Label>Your Balance:</Statistic.Label>
+        <Statistic.Value>2,550.53</Statistic.Value>
+      </Statistic>
+
+      <Segment textAlign="center">
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size="tiny" color="green">
+                <Statistic.Label style={{ textAlign: "left" }}>
+                  Incoming:
+                </Statistic.Label>
+                <Statistic.Value>1,045.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Statistic size="tiny" color="red">
+                <Statistic.Label style={{ textAlign: "left" }}>
+                  Expenses:
+                </Statistic.Label>
+                <Statistic.Value>625.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <header as="h3">History</header>
+
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Something
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $10.00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered></Icon>
+              <Icon name="trash" bordered></Icon>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color="green">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Something else
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $15.00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered></Icon>
+              <Icon name="trash" bordered></Icon>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              new
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $20.00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered></Icon>
+              <Icon name="trash" bordered></Icon>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <header as="h3">Add new transaction</header>
+
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input
+            icon="tags"
+            width={12}
+            label="Description"
+            placeholder="new shiny thing"
+          ></Form.Input>
+          <Form.Input
+            width={4}
+            label="value"
+            placeholder="100"
+            icon="dollar"
+            iconPosition="left"
+          ></Form.Input>
+        </Form.Group>
+
+        <Button.Group style={{marginTop:20}}>
+          <Button>Cancel</Button>
+          <Button.Or />
+          <Button primary>Ok</Button>
+        </Button.Group>
+      </Form>
+    </Container>
   );
 }
 
